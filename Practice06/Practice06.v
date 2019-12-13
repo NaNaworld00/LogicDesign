7a60627f904519b5d95e8be3e8193576072db7c9
NaNaworld00 <43232059+NaNaworld00@users.noreply.github.com>
2019-12-13 21:18:26 +0900

    Update Practice06.v

--- Practice06/Practice06.v
+++ Practice06/Practice06.v
@@ -9,7 +9,7 @@
 //	RELEASE HISTORY
 //	--------------------------------------------------
 //	VERSION			DATE
-//	0.0				2019-10-28
+//	0.0			2019-10-28
 //	--------------------------------------------------
 //	PURPOSE			: Seven-Segment Display
 //	==================================================
@@ -18,14 +18,15 @@
 //	0~59 Counter
 //	--------------------------------------------------
 module cnt60(	o_cnt60,
-				clk,
-				rst_n);
+		clk,
+		rst_n	);
 
 output	[5:0]	o_cnt60		;
-input			clk			;
-input			rst_n		;
+input		clk		;
+input		rst_n		;
 
-reg		[5:0]	o_cnt60		;
+reg	[5:0]	o_cnt60		;
+	
 always @(posedge clk or negedge rst_n) begin
 	if(rst_n == 1'b0) begin
 		o_cnt60 <= 6'd0;
@@ -45,18 +46,19 @@ endmodule
 //	Hz of o_gen_clk = Clock Hz / num
 //	--------------------------------------------------
 module	nco(	o_gen_clk,
-				i_nco_num,
-				clk,
-				rst_n);
+		i_nco_num,
+		clk,
+		rst_n	);
 
-output			o_gen_clk	;	// 1Hz CLK
+output		o_gen_clk	;	// 1Hz CLK
 
 input	[31:0]	i_nco_num	;
-input			clk			;	// 50Mhz CLK
-input			rst_n		;
+input		clk		;	// 50Mhz CLK
+input		rst_n		;
 
-reg		[31:0]	cnt			;
-reg				o_gen_clk	;
+reg	[31:0]	cnt		;
+reg		o_gen_clk	;
+	
 always @(posedge clk or negedge rst_n) begin
 	if(rst_n == 1'b0) begin
 		cnt		<= 32'd0;
@@ -77,26 +79,26 @@ endmodule
 //	NCO Clock Based 0~59 Counter
 //	--------------------------------------------------
 module	nco_cnt(
-				o_nco_cnt,
-				i_nco_num,
-				clk,
-				rst_n);
+		o_nco_cnt,
+		i_nco_num,
+		clk,
+		rst_n		);
 
 output	[5:0]	o_nco_cnt	;
 
 input	[31:0]	i_nco_num	;
-input			clk			;
-input			rst_n		;
+input		clk		;
+input		rst_n		;
 
-wire			gen_clk		;
-nco		u_nco(	.o_gen_clk	( gen_clk	),
-				.i_nco_num	( i_nco_num	),
-				.clk		( clk		),
-				.rst_n		( rst_n		));
+wire		gen_clk		;
+nco	u_nco(	.o_gen_clk	( gen_clk	),
+		.i_nco_num	( i_nco_num	),
+		.clk		( clk		),
+		.rst_n		( rst_n		));
 
 cnt60	u_cnt60(.o_cnt60	( o_nco_cnt	),
-				.clk		( gen_clk	),
-				.rst_n		( rst_n		));
+		.clk		( gen_clk	),
+		.rst_n		( rst_n		));
 
 endmodule
 
@@ -104,7 +106,7 @@ endmodule
 //	Flexible Numerical Display Decoder
 //	--------------------------------------------------
 module	fnd_dec(o_seg,
-				i_num);
+		i_num);
 
 output	[6:0]	o_seg		;	// {o_seg_a, o_seg_b, ... , o_seg_g}
 
@@ -133,9 +135,9 @@ endmodule
 //	0~59 --> 2 Separated Segments
 //	--------------------------------------------------
 module	double_fig_sep(
-				o_left,
-				o_right,
-				i_double_fig);
+			o_left,
+			o_right,
+			i_double_fig);
 
 output	[3:0]	o_left		;
 output	[3:0]	o_right		;
@@ -151,43 +153,46 @@ endmodule
 //	0~59 --> 2 Separated Segments
 //	--------------------------------------------------
 module	led_disp(
-				o_seg,
-				o_seg_dp,
-				o_seg_enb,
-				i_six_digit_seg,
-				i_six_dp,
-				clk,
-				rst_n);
+		o_seg,
+		o_seg_dp,
+		o_seg_enb,
+		i_six_digit_seg,
+		i_six_dp,
+		clk,
+		rst_n);
 
 output	[5:0]	o_seg_enb		;
-output			o_seg_dp		;
+output		o_seg_dp		;
 output	[6:0]	o_seg			;
 
-input	[41:0]	i_six_digit_seg	;
+input	[41:0]	i_six_digit_seg		;
 input	[5:0]	i_six_dp		;
-input			clk				;
-input			rst_n			;
+input		clk			;
+input		rst_n			;
 
-wire			gen_clk		;
+wire		gen_clk			;
+	
 nco		u_nco(	.o_gen_clk	( gen_clk		),
-				.i_nco_num	( 32'd50000	),
-				.clk		( clk			),
-				.rst_n		( rst_n			));
+			.i_nco_num	( 32'd50000		),
+			.clk		( clk			),
+			.rst_n		( rst_n			));
 
 reg		[3:0]	cnt_common_node	;
+	
 always @(posedge gen_clk or negedge rst_n) begin
 	if(rst_n == 1'b0) begin
-		cnt_common_node		<= 32'd0;
+		cnt_common_node	<= 32'd0;
 	end else begin
 		if(cnt_common_node >= 4'd5) begin
-			cnt_common_node 	<= 4'd0;
+			cnt_common_node <= 4'd0;
 		end else begin
 			cnt_common_node <= cnt_common_node + 1'b1;
 		end
 	end
 end
 
-reg		[5:0]	o_seg_enb		;
+reg	[5:0]	o_seg_enb		;
+	
 always @(cnt_common_node) begin
 	case (cnt_common_node)
 		4'd0 : o_seg_enb = 6'b111110;
@@ -199,7 +204,8 @@ always @(cnt_common_node) begin
 	endcase
 end
 
-reg				o_seg_dp		;
+reg		o_seg_dp		;
+	
 always @(cnt_common_node) begin
 	case (cnt_common_node)
 		4'd0 : o_seg_dp = i_six_dp[0];
@@ -211,7 +217,8 @@ always @(cnt_common_node) begin
 	endcase
 end
 
-reg		[6:0]	o_seg			;
+reg	[6:0]	o_seg			;
+	
 always @(cnt_common_node) begin
 	case (cnt_common_node)
 		4'd0 : o_seg = i_six_digit_seg[6:0];
@@ -229,18 +236,18 @@ endmodule
 //	Top Module
 //	--------------------------------------------------
 module	top_nco_cnt_disp(
-				o_seg_enb,
-				o_seg_dp,
-				o_seg,
-				clk,
-				rst_n);
+			o_seg_enb,
+			o_seg_dp,
+			o_seg,
+			clk,
+			rst_n);
 
 output	[5:0]	o_seg_enb		;
-output			o_seg_dp	;
+output		o_seg_dp		;
 output	[6:0]	o_seg			;
 
-input			clk		;
-input			rst_n		;
+input		clk			;
+input		rst_n			;
 
 wire	[5:0]	nco_cnt	;
 nco_cnt		u_nco_cnt(
@@ -248,8 +255,8 @@ nco_cnt		u_nco_cnt(
 				.i_nco_num(32'd500000	),
 				.clk(clk		),
 				.rst_n(rst_n		));
-wire	[3:0]	left	;
-wire	[3:0]	right	;
+wire	[3:0]	left		;
+wire	[3:0]	right		;
 double_fig_sep	u_double_fig_sep(
 				.o_left(left		),
 				.o_right(right		),
